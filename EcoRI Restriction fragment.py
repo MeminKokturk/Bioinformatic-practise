b6409d3dba90254bd49aea7d22959616dcfbc6ae
Muhammed Emin Köktürk <meminkokturk@gmail.com>
2025-08-29 18:34:09 +0300

    Update EcoRI Restriction fragment.py

--- EcoRI Restriction fragment.py
+++ EcoRI Restriction fragment.py
@@ -1,11 +1,22 @@
+import sys
+
+from DNAtoolkit import *
 
 #This program is going to find the EcoRI cutting site and return the cut DNA after the cutting event occured
+
+
 
 dna_seq = input("Enter the sequence that is going to be cut by EcoRI: ")
 
 EcoRI_cut_site = "GAATTC"
 
 dna_seq = dna_seq.upper()
+
+
+if validateSeq(dna_seq) == False:
+    print("This is not a DNA sequence!!!")
+    sys.exit()
+
 
 find_count = 0
 find_count = dna_seq.count(EcoRI_cut_site)
@@ -22,5 +33,4 @@ else:
     dna_seq = dna_seq[position + 1:length]
     position2 = int(dna_seq.find(EcoRI_cut_site))
     print(dna_seq[0:position2+1])
-
 
